59824e9db958388e2e4fb209104e12ee037819ab
Lalith-123 <glalithshivasaiyadav@gmail.com>
2026-03-21 16:32:37 +0530

    Authentication--3

---
+++ src/components/InherenceBased.tsx
@@ -0,0 +1,20 @@
+function InherenceBased() {
+  return (
+    <div className="grid grid-col-1 gap-4 px-40 pb-20 pt-5">
+      <button className="w-60 h-10 border border-black transition-transform duration-200 hover:scale-110 cursor-pointer">
+        Finger Print
+      </button>
+      <button className="w-60 h-10 border border-black transition-transform duration-200 hover:scale-110 cursor-pointer">
+        Face recognition
+      </button>
+      <button className="w-60 h-10 border border-black transition-transform duration-200 hover:scale-110 cursor-pointer">
+        Retina Scan
+      </button>
+      <button className="w-60 h-10 border border-black transition-transform duration-200 hover:scale-110 cursor-pointer">
+        Voice recognition
+      </button>
+    </div>
+  )
+}
+
+export default InherenceBased
